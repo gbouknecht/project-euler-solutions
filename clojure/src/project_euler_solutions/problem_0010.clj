9c37b1eef5961fe4bd83eb83b315ea0cb7745208
Gerrit N. Bouknecht <gbouknecht@server.fake>
2011-09-23 15:07:18 +0200

    Change solution of problem 10 using multiple threads
    The solution of problem 10 can be subdivided in several independent
    tasks.  This solution uses a Java Executor with a fixed thread pool to
    run the tasks. The pool size equals the number of available processors
    (cores). The number of tasks were determined just by trying.

--- clojure/src/project_euler_solutions/problem_0010.clj
+++ clojure/src/project_euler_solutions/problem_0010.clj
@@ -1,8 +1,31 @@
 (ns project-euler-solutions.problem-0010
-  (:use [project-euler-solutions.common]))
+  (:use [project-euler-solutions.common])
+  (:import (java.util.concurrent Executors)))
 
-(defn- sum-of-primes [upper-limit]
-  (apply + (take-while #(< % upper-limit) (primes))))
+(def available-processors (.. Runtime (getRuntime) (availableProcessors)))
+
+(defn- sum-of-primes-between [start end]
+  (apply + (filter prime? (range start end))))
+
+(defn- create-task [upper-limit number-of-tasks task-size task-number]
+  (let [last-task (= (dec number-of-tasks) task-number)
+        start     (* task-size task-number)
+        end       (if last-task upper-limit (+ start task-size))]
+    (fn [] (sum-of-primes-between start end))))
+
+(defn- create-tasks [upper-limit number-of-tasks]
+  (let [task-size (quot upper-limit number-of-tasks)]
+    (map #(create-task upper-limit number-of-tasks task-size %) (range number-of-tasks))))
+
+(defn- sum-of-primes-below
+  ([upper-limit]
+   (sum-of-primes-below upper-limit 1))
+  ([upper-limit number-of-tasks]
+   (let [pool  (Executors/newFixedThreadPool available-processors)
+         tasks (create-tasks upper-limit number-of-tasks)]
+     (try
+       (apply + (map #(.get %) (.invokeAll pool tasks)))
+       (finally (.shutdown pool))))))
 
 (defn answer []
-  (sum-of-primes 2000000))
+  (sum-of-primes-below 2000000 100))
