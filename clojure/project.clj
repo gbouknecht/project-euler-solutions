5def3254e2efefd29d322e51d888b259863307aa
Gerrit N. Bouknecht <gbouknecht@server.fake>
2012-04-30 21:04:19 +0200

    Upgrade to Clojure 1.4.0

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,4 @@
 (defproject project-euler-solutions "0.0.1"
   :description "Project Euler Solutions"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :main project-euler-solutions.core)
