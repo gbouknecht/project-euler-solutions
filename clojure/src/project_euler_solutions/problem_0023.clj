4e41f042f5abb263bb13027dd404ca1f4453b2e4
Gerrit N. Bouknecht <gbouknecht@server.fake>
2012-05-28 20:12:12 +0200

    Change solution of problem 23 using multiple threads

--- clojure/src/project_euler_solutions/problem_0023.clj
+++ clojure/src/project_euler_solutions/problem_0023.clj
@@ -1,5 +1,8 @@
 (ns project-euler-solutions.problem-0023
-  (:use [project-euler-solutions.common]))
+  (:use [project-euler-solutions.common])
+  (:import (java.util.concurrent Executors)))
+
+(def available-processors (.. Runtime (getRuntime) (availableProcessors)))
 
 (defn- abundant-number? [n]
   (> (sum-of-proper-divisors n) n))
@@ -23,8 +26,28 @@
         abundant-numbers-up-to-half-of-n (take-while #(<= % (inc (quot n 2))) abundant-numbers-cached)]
     (not (some #(abundant-number? (- n %)) abundant-numbers-up-to-half-of-n))))
 
-(defn- positive-integers-not-sum-of-two-abundant-numbers []
-  (filter not-sum-of-two-abundant-numbers? (range 1 28124)))
+(defn- sum-between [start end]
+  (reduce + (filter not-sum-of-two-abundant-numbers? (range start end))))
+
+(defn- create-task [upper-limit number-of-tasks task-size task-number]
+  (let [last-task (= (dec number-of-tasks) task-number)
+        start     (* task-size task-number)
+        end       (if last-task upper-limit (+ start task-size))]
+    (fn [] (sum-between start end))))
+
+(defn- create-tasks [upper-limit number-of-tasks]
+  (let [task-size (quot upper-limit number-of-tasks)]
+    (map #(create-task upper-limit number-of-tasks task-size %) (range number-of-tasks))))
+
+(defn- sum
+  ([upper-limit]
+   (sum upper-limit 1))
+  ([upper-limit number-of-tasks]
+   (let [pool  (Executors/newFixedThreadPool available-processors)
+         tasks (create-tasks upper-limit number-of-tasks)]
+     (try
+       (reduce + (map #(.get %) (.invokeAll pool tasks)))
+       (finally (.shutdown pool))))))
 
 (defn answer []
-  (reduce + (positive-integers-not-sum-of-two-abundant-numbers)))
+  (sum 28124 100))
